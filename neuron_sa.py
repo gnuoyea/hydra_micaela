d283ef31890d16c9f749bed4f167fc659d12c1f2
Michael Lin <michael.lin2k21@gmail.com>
2024-12-20 18:43:12 -0800

    QOL changes to surface area

--- neuron_sa.py
+++ neuron_sa.py
@@ -11,10 +11,8 @@ from connectomics.data.utils.data_io import readvol, savevol
 
 if __name__ == '__main__':
     try:
-        neuron= sys.argv[1]
-        # neuron is in resolution [30, 8, 8]. to approximate isotropy, we
-        # dowsample the y and x dimensions by four times
-        neuron = np.clip(readvol(neuron), 0, 1)[:, ::4, ::4]
+        neuron= sys.argv[1] # should have resolution [30, 32, 32]
+        neuron = np.clip(readvol(neuron), 0, 1)# [:, ::4, ::4]
     except Exception as e:
         raise e
 
@@ -22,4 +20,4 @@ if __name__ == '__main__':
     neuron_border = ((neuron_edt>0) * (neuron_edt<=1)).astype(np.uint16)
     voxels = int(neuron_border.sum())
 
-    print(f'surface area (sq. nm): {voxels*32**2}')
+    print(f'{neuron} surface area (sq. nm): {voxels*32**2}')
